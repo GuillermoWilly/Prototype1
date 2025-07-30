2831222c523db49a6dba14771c1a8e1405f7be2a
Guillermo Martin <guillermo.martin@dtn.com>
2025-07-30 19:04:35 +0200

    unified current Prototype1 with vscode extension from Jagalindo

--- extension.js
+++ extension.js
@@ -1,13 +1,33 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+const vscode = require('vscode');
+const path = require('path');
+const { loadPyodide } = require("pyodide");
+
+let globalPyodideInstance = null;
+let packagesLoaded = false;
+
+let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+        statusBarItem.text = "$(sync~spin) Processing...";
+
+
+async function getPyodideInstance(context) {
+    let pyodidePath="file:///"+context.asAbsolutePath(path.join('files','pyodide'));
+    console.log(pyodidePath);
+    if (!globalPyodideInstance) {
+        globalPyodideInstance = await loadPyodide(
+            {
+                indexURL: pyodidePath,
+            }
+        );
+    }
+    return globalPyodideInstance;
+}
+
 function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -21,7 +41,7 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Prototype1!');
+		//vscode.window.showInformationMessage('Hello World from Prototype1!');
 		var panel = vscode.window.createWebviewPanel(
 			'Prototype1',
 			'Vs code prototype for tfg',
